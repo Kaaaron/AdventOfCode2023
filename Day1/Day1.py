91905fb24c275bcbed0bb9a9380dab2ae595c296
Aaron <aaron.kemner@gmail.com>
2023-12-05 20:19:03 +0100

    Day 1 Challenge 2

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,9 +1,8 @@
-
 ########################################
 ######### Day 1, Challenge 1 ###########
 ########################################
 
-def getLineSum(line: str) -> int:
+def getNumOnlyLineSum(line: str) -> int:
 	firstDigit: str = ""
 	lastDigit: str = ""
 
@@ -20,9 +19,58 @@ def getLineSum(line: str) -> int:
 	return int(firstDigit + lastDigit)
 
 
-with open("input.txt") as file:
+with open("Input.txt") as file:
 	outputSum = 0
 	for currentLine in file:
-		outputSum = outputSum + getLineSum(currentLine)
+		outputSum = outputSum + getNumOnlyLineSum(currentLine)
+
+	print(outputSum)  # the final result
+
+########################################
+######### Day 1, Challenge 2 ###########
+########################################
+
+writtenNumbers = "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+
+
+def getStringAndNumLineSum(line: str) -> int:
+	firstDigit: str = ""
+	firstDigitIdx: int = -1
+	lastDigit: str = ""
+	lastDigitIdx: int = -1
+
+	for idx, char in enumerate(line):
+		if char.isnumeric():
+			firstDigit = char
+			firstDigitIdx = idx
+			break
+
+	for idx, char in enumerate(line[::-1]):
+		if char.isnumeric():
+			lastDigit = char
+			lastDigitIdx = len(line) - idx - 1
+			break
+
+	for num in writtenNumbers:
+		foundIdx = line.lower().find(num)
+		if foundIdx != -1:
+			if foundIdx < firstDigitIdx or firstDigitIdx == -1:
+				firstDigit = str(writtenNumbers.index(num) + 1)
+				firstDigitIdx = foundIdx
+
+		rfoundIdx = line.lower().rfind(num)
+		if rfoundIdx != -1:
+			if rfoundIdx > lastDigitIdx or lastDigitIdx == -1:
+				lastDigit = str(writtenNumbers.index(num) + 1)
+				lastDigitIdx = rfoundIdx
+
+	return int(firstDigit + lastDigit)
+
+
+with open("Input.txt") as file:
+	outputSum = 0
+
+	for currentLine in file:
+		outputSum = outputSum + getStringAndNumLineSum(currentLine)
 
 	print(outputSum)  # the final result
